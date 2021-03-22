e1cc14a3fe1ae42efb10b3ee37a83cc931e06d25
Beatriz Milz <beatriz.milz@hotmail.com>
2021-03-22 15:54:25 -0300

    add zen do R, atualiza lista de contrib

--- R/gerar_lista_de_pessoas_contribuidoras.R
+++ R/gerar_lista_de_pessoas_contribuidoras.R
@@ -5,6 +5,7 @@
 #'
 #' @examples
 gerar_lista_contrib <- function(){
+  library(magrittr, include.only = "%>%")
   allcontributors::get_contributors(
     org = "beatrizmilz",
     repo = "materiais_estudo_R",
@@ -13,6 +14,7 @@ gerar_lista_contrib <- function(){
   ) %>%
     tibble::as_tibble() %>%
     dplyr::mutate(url = glue::glue("https://github.com/{logins}")) %>%
+    dplyr::filter(!logins %in% c("actions-user", "beamilz-ensino")) %>%
     readr::write_csv2(file = "inst/lista_de_pessoas_contribuidoras.csv")
 }
 
